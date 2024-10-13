5cafc0631a70b1f740ef61b819e961ba18d66dd1
Pedro Klepa <pedro.klepa@gmail.com>
2024-10-13 21:54:14 +0100

    Remove existing projects & add base Vite TS project

---
+++ apps/vite-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@repo/ui/button'
+
+function App() {
+
+  return (
+    <div>
+        <Button appName='Vite app'>
+          Test Button
+        </Button>
+    </div>
+  )
+}
+
+export default App
